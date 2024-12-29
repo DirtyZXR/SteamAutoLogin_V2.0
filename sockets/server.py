c1fbad1c3c831cfdfb68606102d56518b0074e9d
support@work_denis <deniizz11@yandex.ru>
2024-12-29 16:56:26 +0300

    modify server.py

--- sockets/server.py
+++ sockets/server.py
@@ -1,14 +1,42 @@
 import socket
 import pickle
+from threading import Thread
+import queue
+# import snoop
 
-def server_program():
-    host = socket.gethostname()
-    port = 5000
-    server_socket = socket.socket() # get instance
-    server_socket.bind((host, port))# bind host address and port together
-    server_socket.listen(4)
-    conn, address = server_socket.accept()
-    chat(conn, address)
+
+class ServerSocket:
+    def __init__(self):
+        self.host = socket.gethostname()
+        self.port = 5000
+        self.wait_distribution = queue.Queue()
+        self.wait_guard = queue.Queue()
+        self.socket = socket.socket()
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(25)
+        Thread(target=self.start_accept, daemon=True).start()#todo в клиенте исправить daemon = True
+        Thread(target=self.distribution, daemon=True).start()
+
+
+    def start_accept(self,):
+        while True:
+            conn, address = self.socket.accept()
+            self.wait_distribution.put((conn, address))
+
+
+    def distribution(self):
+        while True:
+            conn, address = self.wait_distribution.get()
+            chunk = conn.recv(1024)#todo try-except
+            data = b'' + chunk
+            data = pickle.loads(data)
+            if data[0] == 'guard':
+                pass
+            elif data[0] == 'ping':
+                pass
+
+            # conn.send(b'1234')
+            # conn.close()
 
 def chat(conn, address,):
     # conn, address = server_socket.accept() # accept new connection
@@ -26,4 +54,4 @@ def chat(conn, address,):
 
 
 if __name__ == '__main__':
-    server_program()
+    ServerSocket()
